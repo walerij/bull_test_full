aece0e1c03d1441c7dc8ce75e91c0ce70196a8d5
walerij <becwal@yandex.ru>
2018-12-22 10:06:45 +0300

    create project bull_test scotch yii2 dbase

---
+++ public/config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=scotchbox',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+
+   
+];
